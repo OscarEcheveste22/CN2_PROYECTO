c7012a8de02d81591270772bdffa166b0b1fb1d3
Oscar Flores <l19251031@sjuanrio.tecnm.mx>
2025-02-28 18:45:09 -0600

    Primer commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+
+app= Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return 'Hola mundo'
+
+@app.route('/alumnos')
+
+def get_alumnos():
+    return 'Retornando todos los alumnos'
+
+if __name__=='__main__':
+    app.run()
