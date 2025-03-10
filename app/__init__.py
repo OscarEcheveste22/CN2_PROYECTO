57ee9e7f72bbb5838e71eb4ce04ff408806f027e
Oscar Flores <l19251031@sjuanrio.tecnm.mx>
2025-03-10 14:16:31 -0600

    Primer commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,34 @@
-from flask import Flask 
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app= Flask(__name__)
+from dotenv import load_dotenv 
 
+# Cargar las variables de entorno
+load_dotenv()
+
+# Crear la instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar la base de datos
+db = SQLAlchemy(app)
+
+# Importar modelos para SQLAlchemy los reconozca
+from app.models import Post, Category
+
+# Importar y registrar blueprints
+from app.routes.post import posts_bp
+from app.routes.category import categories_bp
+
+# Registrar blueprints
+app.register_blueprint(posts_bp, url_prefix='/posts')
+app.register_blueprint(categories_bp, url_prefix='/categories')
+
+# Ruta principal
 @app.route('/')
-
 def index():
-    return 'Hola mundo esto es una prueba'
-
-@app.route('/alumnos')
-
-def get_alumnos():
-    return 'Retornando todos los alumnos'
-
-if __name__=='__main__':
-    app.run()
+    return render_template('home.html')
